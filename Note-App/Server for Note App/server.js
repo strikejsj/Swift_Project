87f715f1bd2a8ea80d3f1638355449cb240a23f3
strikejsj <hv9448@gmail.com>
2021-07-25 14:31:14 +0900

    Add noteSchema

--- Note-App/Server for Note App/server.js
+++ Note-App/Server for Note App/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 var app = express()
+var Data = require('./noteSchema')
+
 
 mongoose.connect('mongodb://localhost/myDB')
 
 mongoose.connection.once("open", () => {
-    
     console.log("Connected to DB!")
-    
+}).on("error", (error) => {
+    console.log("Failed to connect" + error)
 })
